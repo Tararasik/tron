b31452e062d947a45d0e4ae79def0102fec0d6ff
Taras Voitovych <tararasik@gmail.com>
2013-12-24 18:28:45 +0200

    Added zepto

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var Tron = (function() {
 
-	var canvas = document.getElementById('viewport');
+	var canvas = document.getElementById('game');
 	var ctx = canvas.getContext('2d');
 
 	var GAME_OVER = 'over';
@@ -10,143 +10,190 @@ var Tron = (function() {
 	var Game = {
 		options: {
 			speed: 10, // 1s / speed
-			width: 500,
-			//height: 500
+			width: 5
 		},
 
-		state: GAME_OVER,
+		state: GAME_PENDING,
 		mainLoop: null,
+		snakes: [],
 
 		init: function() {
+			Game.snakes = [];
 			canvas.width = Game.options.width;
 			canvas.height = Game.options.height || Game.options.width;
 
 			var options = Game.options;
 
-			Game.clearCanvas();
+			Game.snakes.push(new Snake({
+				startX: canvas.width / 2 + 20
+			}));
+
+			Game.snakes.push(new Snake({
+				color: 'red',
+				keys: {
+					left: 65,
+					up: 87,
+					right: 68,
+					down: 83
+				}
+			}));
+
+			Game.ready();
+		},
+
+		ready: function() {
 			Game.state = GAME_PENDING;
 
-			Snake.init();
+			Game.clearCanvas();
+			$.each(Game.snakes, function(index, snake) {
+				snake.ready();
+			});
 		},
 
 		start: function() {
 			Game.state = GAME_START;
 
-			mainLoop = setInterval(function() {
+			Game.mainLoop = setInterval(function() {
 				if (Game.state == GAME_START) {
-					Snake.move();
+					$.each(Game.snakes, function(index, snake) {
+						snake.move();
+					});
 				}
 			}, 1000 / Game.options.speed);
 		},
 
-		stop: function() {
+		stop: function(player) {
 			Game.state = GAME_OVER;
-			clearInterval(mainLoop);
-			alert("Game over");
+
+			alert(player + ' lost');
+			clearInterval(Game.mainLoop);
 		},
 
 		clearCanvas: function() {
 			ctx.clearRect(0, 0, canvas.width, canvas.height);
+		},
+
+		switchState: function() {
+			switch (Game.state) {
+				case GAME_OVER:
+					Game.ready();
+				case GAME_PENDING:
+					Game.start();
+			}
 		}
 	}
 
-	var Snake = {
-		size: 10, //px 
-		color: 'blue',
-		direction: null,
-		x: canvas.width / 2,
-		y: canvas.height / 2,
+	var Snake = function(options) {
+		this.init(options);
+	};
 
-		init: function() {
-			Snake.x = canvas.width / 2;
-			Snake.y = canvas.height / 2;
-
-			Snake.draw(Snake.x, Snake.y);
-
-			addEventListener("keydown", function (e) {
-		    var lastKey = e.keyCode;
-
-		    switch (lastKey) {
-		    	case 37:
-			    	Snake.direction = 'left';
-			    	if (Game.state != GAME_START) Game.start();
-			    	break;
-		    	case 38:
-			    	Snake.direction = 'up';
-			    	if (Game.state != GAME_START) Game.start();
-			    	break;
-		    	case 39:
-			    	Snake.direction = 'right';
-			    	if (Game.state != GAME_START) Game.start();
-			    	break;
-		    	case 40:
-			    	Snake.direction = 'down';
-			    	if (Game.state != GAME_START) Game.start();
-			    	break;
-		    }
-			}, false);
-		},
-
-		move: function() {
-			switch (Snake.direction) {
-				case 'up':
-					Snake. y -= Snake.size;
-					break;
-				case 'down':
-					Snake.y += Snake.size;
-					break;
-				case 'left':
-					Snake.x -= Snake.size;
-					break;
-				case 'right':
-					Snake.x += Snake.size;
-					break;
-			};
-
-			if (!Snake.isCollision()) {
-				Snake.draw(Snake.x, Snake.y);
+	Snake.prototype.init = function(options) {
+		this.options = {
+			size: 10, //px 
+			color: 'blue',
+			direction: 'up',
+			startX: canvas.width / 2,
+			startY: canvas.height / 2,
+			keys: {
+				left: 37,
+				up: 38,
+				right: 39,
+				down: 40
 			}
-		},
-
-		draw: function(x, y) {
-			var size = Snake.size;
-
-			ctx.fillStyle = Snake.color;
-			ctx.fillRect(x, y, size, size);
-		},
-
-		isCollision: function() {
-			if (Snake.isCrossBorder() || Snake.isCrossSnake()) {
-				Game.stop();
-				return true;
-			}
-
-			return false;
-
-		},
-
-		isCrossBorder: function() {
-			if (Snake.x + Snake.size > canvas.width || Snake.x < 0 || Snake.y + Snake.size > canvas.height + Snake.size || Snake.y < 0) {
-				return true;
-			}
-
-			return false
-		},
-
-		isCrossSnake: function() {
-			var imgData = ctx.getImageData(Snake.x, Snake.y, 1, 1);
-
-			for (i = 0; i < 4; i++) {
-				//if pixel is coloured
-				if (imgData.data[i] != 0) {
-					return true;
-				}
-			}
-
-			return false;
 		}
 
+		if(options) this.options = $.extend(this.options, options);
+
+		var self = this;
+
+		addEventListener("keydown", function(e) {
+			var lastKey = e.keyCode;
+
+	    switch (lastKey) {
+	    	case self.options.keys.left:
+		    	self.options.direction = 'left';
+					Game.switchState();	
+		    	break;
+	    	case self.options.keys.up:
+		    	self.options.direction = 'up';
+					Game.switchState();	
+		    	break;
+	    	case self.options.keys.right:
+		    	self.options.direction = 'right';
+					Game.switchState();	
+		    	break;
+	    	case self.options.keys.down:
+		    	self.options.direction = 'down';
+					Game.switchState();
+		    	break;
+	    }
+		}, false);
+	};
+
+	Snake.prototype.ready = function() {
+		this.options.x = this.options.startX;
+		this.options.y = this.options.startY;
+		this.draw();
 	}
+
+	Snake.prototype.move = function() {
+
+		switch (this.options.direction) {
+			case 'up':
+				this.options.y -= this.options.size;
+				break;
+			case 'down':
+				this.options.y += this.options.size;
+				break;
+			case 'left':
+				this.options.x -= this.options.size;
+				break;
+			case 'right':
+				this.options.x += this.options.size;
+				break;
+		};
+
+		if (!this.isCollision()) {
+			this.draw();
+		}
+	};
+
+	Snake.prototype.draw = function() {
+		ctx.fillStyle = this.options.color;
+		ctx.fillRect(this.options.x, this.options.y, this.options.size, this.options.size);
+	};
+
+	Snake.prototype.isCollision = function() {
+		if (this.isCrossBorder() || this.isCrossSnake()) {
+			Game.stop(this.options.color);
+
+			return true;
+		}
+
+		return false;
+
+	};
+
+	Snake.prototype.isCrossBorder = function() {
+		if (this.options.x + this.size > canvas.width || this.options.x < 0 || this.options.y + this.options.size > canvas.height + this.options.size || this.options.y < 0) {
+			return true;
+		}
+
+		return false
+	};
+
+	Snake.prototype.isCrossSnake = function() {
+		var imgData = ctx.getImageData(this.options.x, this.options.y, 1, 1);
+
+		for (i = 0; i < 4; i++) {
+			//if pixel is coloured
+			if (imgData.data[i] != 0) {
+				return true;
+			}
+		}
+
+		return false;
+	};
 	
 	document.getElementById('start').onclick = Game.init;
 	Game.init();
